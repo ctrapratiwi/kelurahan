dff8fb27c4d4b88dc1d7ebebe33cca810ee8bf80
ctrapratiwi <fcitra10@gmail.com>
2020-06-19 12:33:55 +0800

    initial commit

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Beranda</title>
+<link rel="icon" href="home.png">
+
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+<link rel="stylesheet" type="text/css" href="{{ asset('css/index.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css')}}" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css">
+<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"></script>
+<link rel="stylesheet" href="https://pendataan.baliprov.go.id/assets/frontend/map/MarkerCluster.css" />
+<link rel="stylesheet" href="https://pendataan.baliprov.go.id/assets/frontend/map/MarkerCluster.Default.css" />
+<script src="https://unpkg.com/leaflet-kmz@latest/dist/leaflet-kmz.js"></script>
+  <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+</head>
+
+<body>
+<style>
+#map {
+        height: 400px;
+        width: 100%;
+        padding: 0;
+        margin: 0;
+    }
+</style>
+<div class="container-fluid">
+	<div class="row header">
+    	<div class="col-lg-1">
+			<img src="/image/logo.png" class="img-responsive">
+        </div>
+		<div class="col-lg-10 tittle text-left">
+    		<h2>Persebaran Covid - 19 | Provinsi Bali</h2>
+       	</div>
+
+        <div class="row col-lg-1">
+            <a class="tambahdata" href="/pasien">Tambah Data</a>
+        </div>
+
+   	</div><!-- End Header -->
+
+    <div class="row atas2">
+    	Data Persebaran Kasus Covid-19 :
+    </div>
+
+
+    <div class="row atas">
+		<div class="row col-lg-12">
+
+        	<div class="box2">
+
+                <form action="/search" method="POST" id="form">
+                    @csrf
+                  <div class="form-group">
+                    <label for="from" >Pilih Tanggal Penyebaran :</label>
+                    <input type="date" class="form-control" name="tanggal" id="tanggalSearch"  @if(isset($tanggal)) value="{{$tanggal}}" @endif>
+                  </div>
+
+                   <button type="submit" class="btn btn-success mb-2">Search</button>
+                </form>
+            </div>
+        </div>
+          </div>
+
+        <div class="row atas">
+                <div class="box col-lg-2">
+                    <center><h5 class="title">Positif</h5><center><br>
+                    <h3>{{$totalPositif[0]->total}} Orang</h3><br>
+                    <img src="/image/gambar/covid.png" />
+                </div>
+
+
+                <div class="box col-lg-2">
+                    <center><h5 class="title3">Dirawat</h5><center><br>
+                    <h3>{{$totalDirawat[0]->perawatan}} Orang</h3><br>
+                    <img src="/image/gambar/dirawat.png" />
+                </div>
+
+            	<div class="box col-lg-2">
+                    <center><h5 class="title2">Sembuh</h5><center><br>
+                    <h3>{{$totalSembuh[0]->sembuh}} Orang</h3><br>
+                    <img src="/image/gambar/sembuh.png" />
+                </div>
+
+    			<div class="box col-lg-2">
+                    <center><h5 class="title4">Meninggal</h5><center><br>
+                    <h3>{{$totalMeninggal[0]->meninggal}} Orang</h3><br>
+                    <img src="/image/gambar/meninggal.png" />
+                </div>
+
+        	<div class="card" style="width: 80rem;">
+
+              <div class="card-body">
+                    <h5 class="card-title">Peta Persebaran Kasus Covid-19 di Bali</h5>
+              </div>
+
+              <div id="map"></div>
+        	</div><!-- End Card -->
+
+    	<div class="row text col-lg-12">
+        	<div class="col-lg-6">&copy copyright 2020 Universitas Udayana</div>
+             </div>
+		</div><!-- End Info -->
+	</div> <!-- End Container -->
+
+</body>
+<script src="https://pendataan.baliprov.go.id/assets/frontend/map/leaflet.markercluster-src.js"></script>
+<script>
+$(document).ready(function () {
+    var dataMap=null;
+    var mapcolor=[
+      "128eab",
+      "14a1c2",
+      "21c2e8",
+      "66d5ef",
+      "7ddbf1",
+      "94e2f4",
+      "abe8f6",
+      "c2eef8",
+      "d9f5fb"
+    ];
+    var tanggal = $('#tanggalSearch').val();
+    console.log(tanggal);
+    $.ajax({
+      async:false,
+      url:'getData',
+      type:'get',
+      dataType:'json',
+      data:{date: tanggal},
+      success: function(response){
+        dataMap = response["dataMap"];
+        console.log(dataMap);
+      }
+    });
+    console.log(dataMap);
+
+    // $.ajax({
+    //   async:false,
+    //   url:'getPositif',
+    //   type:'get',
+    //   dataType:'json',
+    //   data:{date: tanggal},
+    //   success: function(response){
+    //     dataPos = response;
+    //   }
+    // });
+    // console.log(dataPos);
+
+    $('#btnGenerateColor').on('click',function(e){
+      var colorStart = $('#colorStart').val();
+      var colorEnd = $('#colorEnd').val();
+      $.ajax({
+        async:false,
+        url:'/create-pallete',
+        type:'get',
+        dataType:'json',
+        data:{start: colorStart, end:colorEnd},
+        success: function(response){
+          mapcolor = response;
+          setMapColor();
+        }
+      });
+
+    });
+
+    var map = L.map('map');
+    map.setView(new L.LatLng(-8.374187,115.172922), 10);
+
+    var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      maxZoom: 17,
+      attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+      opacity: 0.90
+    });
+
+    OpenTopoMap.addTo(map);
+    setMapColor();
+    // define variables
+    var lastLayer;
+    var defStyle = {opacity:'1',color:'#000000',fillOpacity:'0',fillColor:'#CCCCCC'};
+    var selStyle = {color:'#0000FF',opacity:'1',fillColor:'#00FF00',fillOpacity:'1'};
+
+    function setMapColor(){
+      var markerIcon = L.icon({
+        iconUrl: '/mar.png',
+        iconSize: [40, 60],
+      });
+
+    // Instantiate KMZ parser (async)
+    var kmzParser = new L.KMZParser({
+        onKMZLoaded: function (kmz_layer, name) {
+          control.addOverlay(kmz_layer, name);
+          var markers = L.markerClusterGroup();
+          var layers = kmz_layer.getLayers()[0].getLayers();
+
+            // fetching sub layer
+      	  layers.forEach(function(layer, index){
+
+            var kab  = layer.feature.properties.NAME_2;
+                var kec =  layer.feature.properties.NAME_3;
+                var kel = layer.feature.properties.NAME_4;
+                var data;
+
+                var STYLE = {opacity:'1',color:'#000',fillOpacity:'1'};
+                var HIJAU_MUDA = {opacity:'1',color:'#000',fillOpacity:'1', fillColor:'#81F781'};
+                var HIJAU_TUA = {opacity:'1',color:'#000',fillOpacity:'1', fillColor:'#088A08'};
+                var KUNING = {opacity:'1',color:'#000',fillOpacity:'1', fillColor:'#FFFF00'};
+                var MERAH_MUDA = {opacity:'1',color:'#000',fillOpacity:'1', fillColor:'#F78181'};
+                var MERAH_TUA = {opacity:'1',color:'#000',fillOpacity:'1', fillColor:'#B40404'};
+                if(!Array.isArray(dataMap) || !dataMap.length == 0){
+                // set sub layer default style positif covid
+                  // var STYLE = {opacity:'1',color:'#000',fillOpacity:'1',fillColor:'#'+mapcolor[index]};
+                  // layer.setStyle(STYLE);
+                    var searchResult = dataMap.filter(function(it){
+                      return it.kecamatan.replace(/\s/g,'').toLowerCase() === kec.replace(/\s/g,'').toLowerCase() &&
+                              it.kelurahan.replace(/\s/g,'').toLowerCase() === kel.replace(/\s/g,'').toLowerCase();
+                    });
+                    if(!Array.isArray(searchResult) || !searchResult.length ==0){
+                      var item = searchResult[0];
+                      if(item.total == 0 ){
+                        layer.setStyle(HIJAU_MUDA);
+                      }else if(item.perawatan == 0 && item.total>0 && item.sembuh >= 0 && item.meninggal >=0){
+                        layer.setStyle(HIJAU_TUA);
+                      }else if(item.ppln ==1 && item.perawatan == 1 && item.total == 1 && item.tl==0 || item.ppdn ==1 && item.perawatan == 1 && item.total == 1 && item.tl==0){
+                        layer.setStyle(KUNING);
+                      }else if((item.ppln >1 && item.perawatan <= item.ppln && item.sembuh <= item.ppln && item.tl == 0) || (item.ppdn >1 && item.perawatan <= item.ppdn && item.sembuh <= item.ppdn && item.tl == 0)  ){
+                        layer.setStyle(MERAH_MUDA);
+                      }else{
+                        layer.setStyle(MERAH_TUA);
+                      }
+                      data = '<table width="300">';
+                      data +='  <tr>';
+                      data +='    <th colspan="2">Keterangan</th>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Kabupaten</td>';
+                      data +='    <td>: '+kab+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr >';
+                      data +='    <td>Kecamatan</td>';
+                      data +='    <td>: '+kec+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Kelurahan</td>';
+                      data +='    <td>: '+kel+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>PP-LN</td>';
+                      data +='    <td>: '+item.ppln+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>PP-DN</td>';
+                      data +='    <td>: '+item.ppdn+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>TL</td>';
+                      data +='    <td>: '+item.tl+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Lainnya</td>';
+                      data +='    <td>: '+item.lainnya+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr style="color:green">';
+                      data +='    <td>Sembuh</td>';
+                      data +='    <td>: '+item.sembuh+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr style="color:blue">';
+                      data +='    <td>Dalam Perawatan</td>';
+                      data +='    <td>: '+item.perawatan+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr style="color:red">';
+                      data +='    <td>Meninggal</td>';
+                      data +='    <td>: '+item.meninggal+'</td>';
+                      data +='  </tr>';
+                    }else{
+                      console.log(kel.replace(/\s/g,'').toLowerCase());
+                      console.log(kec.replace(/\s/g,'').toLowerCase());
+                      data = '<table width="300">';
+                      data +='  <tr>';
+                      data +='    <th colspan="2">Keterangan</th>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Kabupaten</td>';
+                      data +='    <td>: '+kab+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr style="color:red">';
+                      data +='    <td>Kecamatan</td>';
+                      data +='    <td>: '+kec+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr style="color:red">';
+                      data +='    <td>Kelurahan</td>';
+                      data +='    <td>: '+kel+'</td>';
+                      data +='  </tr>';
+                    }
+
+                }else{
+                  // var data = "Tidak ada Data pada tanggal tersebut"
+                  layer.setStyle(defStyle);
+                  data = '<table width="300">';
+                      data +='  <tr>';
+                      data +='    <th colspan="2">Keterangan</th>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Kabupaten</td>';
+                      data +='    <td>: '+kab+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Kecamatan</td>';
+                      data +='    <td>: '+kec+'</td>';
+                      data +='  </tr>';
+
+                      data +='  <tr>';
+                      data +='    <td>Kelurahan</td>';
+                      data +='    <td>: '+kel+'</td>';
+                      data +='  </tr>';
+                }
+                layer.bindPopup(data);
+                // markers.addLayer(L.marker(getRandomLatLng(map)));
+                markers.addLayer(
+                  L.marker(layer.getBounds().getCenter(),{
+                    icon: markerIcon
+                  }).bindPopup(data)
+                );
+              });
+              map.addLayer(markers);
+              kmz_layer.addTo(map);
+        }
+    });
+
+    // Add remote KMZ files as layers (NB if they are 3rd-party servers, they MUST have CORS enabled)
+    kmzParser.load('bali-kelurahan.kmz');
+    // kmzParser.load('https://raruto.github.io/leaflet-kmz/examples/globe.kmz');
+
+    var control = L.control.layers(null, null, {
+        collapsed: false
+    }).addTo(map);
+    $('.leaflet-control-layers').hide();
+    }
+  });
+</script>
+</html>
