ee5d072588ae769cfa95857366824d546c4c02cc
ctrapratiwi <49809219+ctrapratiwi@users.noreply.github.com>
2020-06-19 12:39:32 +0800

    Update index.blade.php

--- resources/views/Pasien/index.blade.php
+++ resources/views/Pasien/index.blade.php
@@ -6,8 +6,8 @@
 <link rel="icon" href="tambah.png">
 
 <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
-<link rel="stylesheet" type="text/css" href="{{ asset('css/tambahdata.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/tambahdata.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/bootstrap.css')}}" />
 
 </head>
 
